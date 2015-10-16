7d0e36f9d75de9d7517266f0b10b726826e4a9a3
bbalet <benjamin.balet@gmail.com>
2015-10-16 21:37:59 +0200

    Update the POC so as to use stopwords algo
    We now rely on github.com/bbalet/stopwords

--- gorelated.go
+++ gorelated.go
@@ -1,4 +1,5 @@
 package main
+
 /*  gorelated finds the list of related text files from a folder
 	Copyright (C) 2013 Benjamin BALET
 
@@ -14,8 +15,8 @@ package main
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+
 import (
-	//"bytes"
 	"crypto/md5"
 	"flag"
 	"fmt"
@@ -27,42 +28,43 @@ import (
 	"regexp"
 	"sort"
 	"time"
-	"bitbucket.org/kardianos/osext"
-	"code.google.com/p/go.text/unicode/norm"
+
+	"github.com/bbalet/stopwords"
+	"github.com/kardianos/osext"
 	"github.com/mfonda/simhash"
+	"golang.org/x/text/unicode/norm"
 )
 
 // Definitions used by sort package
 type articleList []article
+
 func (a articleList) Len() int {
-    return len(a)
+	return len(a)
 }
 func (a articleList) Swap(i, j int) {
-    a[i], a[j] = a[j], a[i]
+	a[i], a[j] = a[j], a[i]
 }
 func (a articleList) Less(i, j int) bool {
-    return a[i].Score < a[j].Score
+	return a[i].Score < a[j].Score
 }
 
 // An article
 type article struct {
-	Key			string		//MD5 hash of its path
-	Path		string		//Article full path 
-	SimHash		uint64		//Hash representing the content
-	Score		uint8		//Distance to another article
-	Related 	articleList	//List of related articles sorted out by their scores
+	Key     string      //MD5 hash of its path
+	Path    string      //Article full path
+	SimHash uint64      //Hash representing the content
+	Score   uint8       //Distance to another article
+	Related articleList //List of related articles sorted out by their scores
 }
 
 var (
 	// Command line flags
-    input = flag.String("input", "input", "Input folder")
+	input      = flag.String("input", "input/en", "Input folder")
 	extensions = flag.String("extensions", ".*\\.html", "Regexp matching files to be added to the list")
-	length = flag.Int("length", 5, "Number of related files to be displayed")
+	length     = flag.Int("length", 5, "Number of related files to be displayed")
+	langCode   = flag.String("lang", "en", "ISO 639-1 language code of the content")
 	// Global vars
 	extRule *regexp.Regexp
-	// Regular expressions
-	remTags = regexp.MustCompile(`<[^>]*>`)
-	oneSpace = regexp.MustCompile(`\s{2,}`)
 	// Working directory
 	inPath string
 	//Cache of articles
@@ -73,10 +75,9 @@ var (
 func main() {
 
 	start := time.Now()
-
 	flag.Parse()
 	exePath, _ := osext.ExecutableFolder()
-	
+
 	//Check if input folder exists
 	if !filepath.IsAbs(*input) {
 		inPath = filepath.Join(exePath, *input)
@@ -86,7 +87,7 @@ func main() {
 	if _, err := os.Stat(inPath); os.IsNotExist(err) {
 		log.Fatal("no such directory: ", inPath)
 	}
-	
+
 	//Recursively Browse a folder to find all files
 	log.Println("Reading and analyzing all input files...")
 	cache = make(map[string]article)
@@ -96,15 +97,15 @@ func main() {
 		log.Fatal("I Can't browse recursively this folder : ", inPath)
 	}
 	log.Println("Reading time : ", time.Since(start))
-	
+
 	log.Println("Comparing all input files between one another...")
 	compareFiles()
 	log.Println("Total time : ", time.Since(start))
-	
+
 	log.Println("Below, the results ")
 	for _, v := range cache {
 		fmt.Println("---;File;", filepath.Base(v.Path))
-		results := getRelatedPosts(v.Path, *length)		//A bit silly to call this function here...
+		results := getRelatedPosts(v.Path, *length) //A bit silly to call this function here...
 		for _, item := range results {
 			fmt.Println("Distance;File;", item.Score, filepath.Base(item.Path))
 		}
@@ -131,13 +132,13 @@ func compareFiles() {
 		for k, v := range cache {
 			if k != baseKey {
 				r := article{Key: v.Key,
-							Path: v.Path,
-							Score: simhash.Compare(cache[baseKey].SimHash,
-													cache[k].SimHash)}
+					Path: v.Path,
+					Score: simhash.Compare(cache[baseKey].SimHash,
+						cache[k].SimHash)}
 				listOfArticles = append(listOfArticles, r)
 			}
 		}
-		
+
 		//Sort the results by their relative scores
 		sort.Sort(articleList(listOfArticles))
 		//Query, and assign back... (see https://code.google.com/p/go/issues/detail?id=3117)
@@ -166,12 +167,11 @@ func visit(path string, f os.FileInfo, err error) error {
 				log.Fatal("I Can't read this file : ", path)
 			}
 			// Remove all HTML tags, simplify the content and calculate the hash
-			content = remTags.ReplaceAll(content, []byte(" "))
-			content = oneSpace.ReplaceAll(content, []byte(" "))
+			cleanContent := stopwords.CleanContent(string(content), *langCode, true)
 			// NB: tolower is done by the Unicode/normalization
-			a := article{Key:name2key(path),
-						Path: path,
-						SimHash: simhash.Simhash(simhash.NewUnicodeWordFeatureSet(content, norm.NFKC))}
+			a := article{Key: name2key(path),
+				Path:    path,
+				SimHash: simhash.Simhash(simhash.NewUnicodeWordFeatureSet([]byte(cleanContent), norm.NFKC))}
 			cache[a.Key] = a
 		}
 	}
--- readme.md
+++ readme.md
@@ -7,9 +7,11 @@ It comes with some HTML files coming from Wikipedia or the public domain.
 * **input**  Input folder (default: "input")
 * **extensions** Regexp matching files to be added to the list(default: ".*\\.html")
 * **length** Number of related files to be displayed (default: 5)
+* **langCode** ISO 639-1 language code of the content (default: en)
 
 # Dependancies
 
-* bitbucket.org/kardianos/osext get the folder where the executable is installed (not running)
-* code.google.com/p/go.text/unicode/norm Unicode normalization
+* github.com/kardianos/osext get the folder where the executable is installed (not running)
+* golang.org/x/text/unicode/norm Unicode normalization
 * github.com/mfonda/simhash Calculate a hash reprensenting a text
+* github.com/bbalet/stopwords Libray cleaning the stop words, HTML tags and duplicated spaces in a content
